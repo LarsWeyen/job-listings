fdbf56cd621f71e2d10d1fd1a89e1aff79179868
LarsWeyen <97236276+LarsWeyen@users.noreply.github.com>
2024-02-13 13:19:35 +0100

    feat: added job model class

---
+++ backend/JobListingsApi/Models/Job.cs
@@ -0,0 +1,15 @@
+﻿namespace JobListingsApi.Models
+{
+    public class Job
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Image {  get; set; }
+        public string Company { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public string Type { get; set; }
+        public DateTime Created_date { get; set; }
+        public List<string> Tags { get; set; }
+    }
+}
