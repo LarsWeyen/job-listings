49148718e7700cd4cfd1744b57bf5d1b98823078
LarsWeyen <97236276+LarsWeyen@users.noreply.github.com>
2024-02-13 13:22:21 +0100

    feat: created jobcontroller and a simple get to retrieve all jobs in database

---
+++ backend/JobListingsApi/Controllers/JobsController.cs
@@ -0,0 +1,29 @@
+﻿using JobListingsApi.Data;
+using JobListingsApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobListingsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobsController : ControllerBase
+    {
+        private readonly ApiDbContext _context;
+
+        public JobsController(ApiDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet(Name = "GetAllJobs")]
+        public async Task<IActionResult> Get()
+        {
+            
+
+            var allJobs = await _context.Jobs.ToListAsync();
+
+            return Ok(allJobs);
+        }
+    }
+}
